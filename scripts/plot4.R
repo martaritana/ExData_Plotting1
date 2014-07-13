a606ad3dec18f6e923b5122414cd5ea7d13d7404
Margarette Kulikova <margarita2088@gmail.com>
2014-07-14 03:20:59 +0400

    plot 4 legend border removed

--- scripts/plot4.R
+++ scripts/plot4.R
@@ -11,10 +11,11 @@ axis(1, c(1, nrow(hpc) / 2, nrow(hpc)), c("Thu", "Fri", "Sat"))
 plot(hpc$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering", xaxt="n", col="black")
 lines(hpc$Sub_metering_2, col="red")
 lines(hpc$Sub_metering_3, col="blue")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), pch="—")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), pch="—", bty="n")
 axis(1, c(1, nrow(hpc) / 2, nrow(hpc)), c("Thu", "Fri", "Sat"))
 
 plot(hpc$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global Reactive Power", xaxt="n")
 axis(1, c(1, nrow(hpc) / 2, nrow(hpc)), c("Thu", "Fri", "Sat"))
 
 dev.off()
+
