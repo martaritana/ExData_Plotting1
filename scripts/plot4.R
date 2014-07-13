fdd824a73f63bd2324c9c17d7b29ac2fd523a896
Margarette Kulikova <margarita2088@gmail.com>
2014-07-14 03:18:09 +0400

    plot 4

---
+++ scripts/plot4.R
@@ -0,0 +1,20 @@
+hpc <- read.csv("data/hpc.csv")
+png(filename = "plots/plot4.png", width = 480, height = 480)
+par(mfrow = c(2,2))
+
+plot(hpc$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)", xaxt="n")
+axis(1, c(1, nrow(hpc) / 2, nrow(hpc)), c("Thu", "Fri", "Sat"))
+
+plot(hpc$Voltage, type = "l", xlab = "datetime", ylab = "Voltage", xaxt="n")
+axis(1, c(1, nrow(hpc) / 2, nrow(hpc)), c("Thu", "Fri", "Sat"))
+
+plot(hpc$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering", xaxt="n", col="black")
+lines(hpc$Sub_metering_2, col="red")
+lines(hpc$Sub_metering_3, col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), pch="—")
+axis(1, c(1, nrow(hpc) / 2, nrow(hpc)), c("Thu", "Fri", "Sat"))
+
+plot(hpc$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global Reactive Power", xaxt="n")
+axis(1, c(1, nrow(hpc) / 2, nrow(hpc)), c("Thu", "Fri", "Sat"))
+
+dev.off()
